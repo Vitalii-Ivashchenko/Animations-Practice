0fbcb020a769ae40483265c7757c5e20b5c80906
Vitalii Ivashchenko <vi.algorithm@gmail.com>
2021-04-15 17:33:22 +0300

    Initial project

---
+++ Animations-Practice/Animations-Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Animations-Practice
+//
+//  Created by Віталій on 15.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
